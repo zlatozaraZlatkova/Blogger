e06c36d58b309539c09d0e3ad7d6081ebd319241
Zara <zl.zlatkova@gmail.com>
2025-07-01 16:22:03 +0200

    Refactor:  Add 401 check and error forwarding in interceptor

--- client/src/app/core/interceptors/error.interseptor.ts
+++ client/src/app/core/interceptors/error.interseptor.ts
@@ -5,40 +5,46 @@ import {
     HttpHandler,
     HttpInterceptor,
     HttpRequest,
+    HttpResponse,
 } from '@angular/common/http';
 import { Injectable, Provider } from '@angular/core';
-import { catchError, EMPTY, Observable } from 'rxjs';
+import { catchError, EMPTY, Observable, of, throwError } from 'rxjs';
 import { ErrorService } from 'src/app/shared/error-notification/error.service';
 
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-    constructor(private errorService: ErrorService) {}
+    constructor(private errorService: ErrorService) { }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
         return next.handle(req)
             .pipe(
                 catchError((error: HttpErrorResponse) => {
-                    
-                    if (error.status === 401) {
-                       return EMPTY //of(new HttpResponse({ status: 401, body: null })) as Observable<HttpEvent<any>>;
+
+                    if (error.status === 401 && req.url.includes('/check-auth')) {
+                        return of(new HttpResponse({ status: 401, body: null })) as Observable<HttpEvent<any>>;
                     }
 
-                    let errorMessage = '';
-                    if (error.error && error.error.message) {
-                        errorMessage = error.error.message;
-                    } else {
-                        errorMessage = error.message;
-                    }
-
+                    let errorMessage = this.extractServerMsg(error);
                     this.errorService.setError(errorMessage);
 
-                    return EMPTY; //return throwError(() => new Error(errorMessage));
-                    
+                    //return throwError(() => new Error(errorMessage));
+                    return EMPTY
+                 
                 })
             );
     }
+
+    private extractServerMsg(error: HttpErrorResponse): string {
+
+        if (error.error && error.error.message) {
+            return error.error.message;
+        } else {
+            return error.message;
+        }
+    }
+
 }
 
 export const errorInterceptor: Provider = {
