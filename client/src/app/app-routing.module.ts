c5e70ba66f2b29c35faaa7770cd0d234ac2670aa
Zara <zl.zlatkova@gmail.com>
2025-07-06 17:49:30 +0200

    Fix date format and add scroll restoration:
    - Fix date pipe format mm→MM for correct month display
    - Add scrollPositionRestoration: 'top' for better navigation UX

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -39,7 +39,12 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    RouterModule.forRoot(routes, {
+      scrollPositionRestoration: 'top'
+    }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule { }
