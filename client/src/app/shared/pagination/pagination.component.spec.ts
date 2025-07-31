b7fa592f6582990926056b1a99658d29d0977b8c
Zara <zl.zlatkova@gmail.com>
2025-07-31 11:53:16 +0200

    Add unit tests for dynamic page generation and conditional styling

--- client/src/app/shared/pagination/pagination.component.spec.ts
+++ client/src/app/shared/pagination/pagination.component.spec.ts
@@ -107,6 +107,91 @@ describe('PaginationComponent', () => {
 
 
 
+  it('should display all pages', () => {
+
+    component.paginationData = {
+      hasPrevPage: true,
+      hasNextPage: true,
+      currentPage: 3,
+      totalPages: 5,
+      totalItems: 25
+    } as IPagination
 
 
+    fixture.detectChanges();
+
+    const pageNumbers = component.getPageNumbers();
+
+    expect(pageNumbers).toEqual([1, 2, 3, 4, 5]);
+
+
+  });
+
+
+
+  it('should display current page button (page 3)', () => {
+
+    component.paginationData = {
+      hasPrevPage: true,
+      hasNextPage: true,
+      currentPage: 3,
+      totalPages: 5,
+      totalItems: 25
+    } as IPagination
+
+
+    fixture.detectChanges();
+
+    const buttons = fixture.nativeElement.querySelectorAll('button.page-button') as NodeListOf<HTMLButtonElement>;
+
+    const activeButton = Array.from(buttons).find(btn => btn.textContent?.trim() === '3');
+
+    expect(activeButton?.textContent?.trim()).toEqual('3');
+  });
+
+
+  it('should apply active class to the current page button (page 3)', () => {
+
+    component.paginationData = {
+      hasPrevPage: true,
+      hasNextPage: true,
+      currentPage: 3,
+      totalPages: 5,
+      totalItems: 25
+    } as IPagination
+
+
+    fixture.detectChanges();
+
+    const buttons = fixture.nativeElement.querySelectorAll('button.page-button') as NodeListOf<HTMLButtonElement>;
+
+    const activeButton = Array.from(buttons).find(btn => btn.textContent?.trim() === '3')
+
+
+    expect(activeButton?.classList).toContain('page-button--active');
+  });
+
+
+  it('should apply inactive class to non-current page button (page 1)', () => {
+
+    component.paginationData = {
+      hasPrevPage: true,
+      hasNextPage: true,
+      currentPage: 3,
+      totalPages: 5,
+      totalItems: 25
+    } as IPagination
+
+
+    fixture.detectChanges();
+
+    const buttons = fixture.nativeElement.querySelectorAll('button.page-button') as NodeListOf<HTMLButtonElement>;
+
+    const notActiveButton = Array.from(buttons).find(btn => btn.textContent?.trim() !== '3')
+
+
+    expect(notActiveButton?.classList).toContain('page-button--inactive');
+  });
+
+  
 });
