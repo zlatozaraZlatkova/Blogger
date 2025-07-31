fe3e49343eabbee701edee3986a219655cfb191e
Zara <zl.zlatkova@gmail.com>
2025-07-31 16:27:02 +0200

    Add unit tests with router.navigate and spyOn:
    - Replace [routerLink] with (click) handler to enable unit testing

--- client/src/app/core/header/header.component.spec.ts
+++ client/src/app/core/header/header.component.spec.ts
@@ -3,6 +3,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HeaderComponent } from './header.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -11,7 +13,7 @@ describe('HeaderComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [HeaderComponent],
-      imports: [HttpClientTestingModule],
+      imports: [HttpClientTestingModule, RouterTestingModule],
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
     });
     fixture = TestBed.createComponent(HeaderComponent);
@@ -22,4 +24,40 @@ describe('HeaderComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
+  it('should navigate to posts when blog link is clicked', () => {
+    const router = TestBed.inject(Router);
+    spyOn(router, 'navigate');
+
+    component.toggleMobileMenu();
+    fixture.detectChanges();
+
+
+    const blogLink = fixture.nativeElement.querySelector('.blogLink');
+    blogLink.click();
+
+
+
+    expect(router.navigate).toHaveBeenCalledWith(['/posts']);
+  });
+
+
+  it('should navigate to posts/create when blog link is clicked', () => {
+    const router = TestBed.inject(Router);
+    spyOn(router, 'navigate');
+
+    component.toggleMobileMenu();
+    fixture.detectChanges();
+
+
+    const writeLink = fixture.nativeElement.querySelector('.writeLink');
+    writeLink.click();
+
+
+
+    expect(router.navigate).toHaveBeenCalledWith(['/posts/create']);
+  });
+
+
 });
--- client/src/app/core/header/header.component.ts
+++ client/src/app/core/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from 'src/app/user/auth.service';
 
 @Component({
@@ -13,7 +14,10 @@ export class HeaderComponent {
     return this.authService.isLoggedIn;
   }
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
   toggleMobileMenu(): void {
     this.isMobileMenuOpen = !this.isMobileMenuOpen;
@@ -28,5 +32,13 @@ export class HeaderComponent {
   closeMobileMenu(): void {
     this.isMobileMenuOpen = false;
     document.body.classList.remove('overflow-hidden');
+  }
+
+  navigateToPosts(): void {
+    this.router.navigate(['/posts'])
+  }
+
+  navigateToCreatePost(): void {
+    this.router.navigate(['/posts/create'])
   }
 }
