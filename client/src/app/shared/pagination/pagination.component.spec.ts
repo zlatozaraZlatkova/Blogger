c1cd16aff161d99bb653498e9369ff319e632450
Zara <zl.zlatkova@gmail.com>
2025-07-31 10:33:41 +0200

    Add unit tests disabled state for Previous/Next buttons based on hasPrevPage/hasNextPage

--- client/src/app/shared/pagination/pagination.component.spec.ts
+++ client/src/app/shared/pagination/pagination.component.spec.ts
@@ -31,4 +31,82 @@ describe('PaginationComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
+  it('should set disabled attribute when paginationData has NO PrevPage', () => {
+
+    component.paginationData = {
+      hasPrevPage: false,
+      hasNextPage: true,
+      currentPage: 1,
+      totalPages: 1,
+      totalItems: 10
+    } as IPagination
+
+    fixture.detectChanges();
+
+    const button = fixture.nativeElement.querySelector('button.previous.nav-button');
+
+    expect(button.disabled).toBe(true);
+  });
+
+
+  it('should NOT set disabled attribute when paginationData HAS PrevPage', () => {
+
+    component.paginationData = {
+      hasPrevPage: true,
+      hasNextPage: true,
+      currentPage: 2,
+      totalPages: 3,
+      totalItems: 10
+    } as IPagination
+
+    fixture.detectChanges();
+
+    const button = fixture.nativeElement.querySelector('button.previous.nav-button');
+
+    expect(button.disabled).toBe(false);
+  });
+
+
+
+  it('should set disabled attribute when paginationData has NOT NextPage', () => {
+
+    component.paginationData = {
+      hasPrevPage: true,
+      hasNextPage: false,
+      currentPage: 2,
+      totalPages: 2,
+      totalItems: 10
+    } as IPagination
+
+    fixture.detectChanges();
+
+    const button = fixture.nativeElement.querySelector('button.next.nav-button');
+
+    expect(button.disabled).toBe(true);
+  });
+
+
+  it('should NOT set disabled attribute when paginationData HAS NextPage', () => {
+
+    component.paginationData = {
+      hasPrevPage: true,
+      hasNextPage: true,
+      currentPage: 1,
+      totalPages: 2,
+      totalItems: 10
+    } as IPagination
+
+    fixture.detectChanges();
+
+    const button = fixture.nativeElement.querySelector('button.next.nav-button')
+
+    expect(button.disabled).toBe(false);
+  });
+
+
+
+
+
 });
