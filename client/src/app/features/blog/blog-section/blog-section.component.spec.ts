dd76b69989eed28c820ec6b3da6b1b09f5d7853b
Zara <zl.zlatkova@gmail.com>
2025-08-03 16:50:23 +0200

    Add unit tests for posts rendering

--- client/src/app/features/blog/blog-section/blog-section.component.spec.ts
+++ client/src/app/features/blog/blog-section/blog-section.component.spec.ts
@@ -5,6 +5,12 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute } from '@angular/router';
 import { of } from 'rxjs';
+import { BlogCardComponent } from '../blog-card/blog-card.component';
+import { BlogService } from '../blog.service';
+
+import { SharedModule } from 'src/app/shared/shared.module';
+import { IPost, IPostsResponse } from 'src/app/interfaces/post';
+import { IPagination } from 'src/app/interfaces/pagination';
 
 describe('BlogSectionComponent', () => {
   let component: BlogSectionComponent;
@@ -12,20 +18,22 @@ describe('BlogSectionComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [BlogSectionComponent],
-      imports: [HttpClientTestingModule],
+      declarations: [BlogSectionComponent, BlogCardComponent],
+      imports: [HttpClientTestingModule, SharedModule],
       providers: [
+        BlogService,
         { provide: MatDialog, useValue: { open: () => { } } },
         {
           provide: ActivatedRoute,
           useValue: {
             queryParams: of({}),
             params: { subscribe: () => { } },
-            snapshot: { params: {} }
-          }
-        }
-      ]
+            snapshot: { params: {} },
+          },
+        },
+      ],
     });
+
     fixture = TestBed.createComponent(BlogSectionComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -34,4 +42,79 @@ describe('BlogSectionComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display posts when data is available', () => {
+    const blogService = TestBed.inject(BlogService);
+
+    const mockPostsData = {
+      success: true,
+      data: {
+        items: [
+          {
+            _id: '1',
+            name: 'Test User',
+            avatar: 'www.avatar.com',
+            postImageUrl: 'www.url.com',
+            postCategory: '',
+            postTags: ['angular', 'nodejs', 'typescript'],
+            postTitle: 'Post Title',
+            postText: 'Post Text',
+            postLikes: [],
+            comments: [],
+            views: 88,
+            ownerId: '01',
+          } as IPost,
+        ],
+        pagination: {
+          hasPrevPage: false,
+          hasNextPage: false,
+          currentPage: 1,
+          totalPages: 1,
+          totalItems: 1,
+        } as IPagination,
+      },
+    } as IPostsResponse;
+
+    blogService.paginatedPosts$ = of(mockPostsData);
+
+    fixture = TestBed.createComponent(BlogSectionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+
+    const allPosts = fixture.nativeElement.querySelectorAll(
+      '[data-testid="post"]'
+    );
+    expect(allPosts.length).toBe(1);
+  });
+
+  it('should NOT display posts data is NOT available', () => {
+    const blogService = TestBed.inject(BlogService);
+
+    const emptyPostData = {
+      success: true,
+      data: {
+        items: [],
+        pagination: {
+          hasPrevPage: false,
+          hasNextPage: false,
+          currentPage: 1,
+          totalPages: 1,
+          totalItems: 0,
+        } as IPagination,
+      },
+    } as IPostsResponse;
+
+    blogService.paginatedPosts$ = of(emptyPostData);
+
+    fixture = TestBed.createComponent(BlogSectionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+
+    const allPosts = fixture.nativeElement.querySelectorAll(
+      '[data-testid="post"]'
+    );
+    expect(allPosts.length).toBe(0);
+  });
+
+
 });
