819593ed9567db27969905a5f6e4d5b54830c141
Zara <zl.zlatkova@gmail.com>
2025-08-07 15:39:21 +0200

    Add mode tests for ProfileFormDialogComponent:
    - Setup MAT_DIALOG_DATA provider in TestBed and mock data
    - Test ngOnInit logic for edit/create mode detection

--- client/src/app/features/profile/profile-form-dialog/profile-form-dialog.component.spec.ts
+++ client/src/app/features/profile/profile-form-dialog/profile-form-dialog.component.spec.ts
@@ -5,20 +5,43 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { IProfile } from 'src/app/interfaces/profile';
+import { IDialogData } from 'src/app/interfaces/dialogData';
 
 describe('ProfileFormDialogComponent', () => {
   let component: ProfileFormDialogComponent;
   let fixture: ComponentFixture<ProfileFormDialogComponent>;
+  let mockDialogRefSpy: jasmine.SpyObj<MatDialogRef<ProfileFormDialogComponent>>;
+
+  const mockModeCreateData: IDialogData = {
+    mode: 'create',
+    data: undefined,
+    title: 'Create Public Profile'
+  };
+
+  const mockModeEditData: IDialogData = {
+    mode: 'edit',
+    data: {
+      bio: 'Test bio content',
+      githubUsername: 'testuser',
+      socialMedia: {
+        linkedin: 'testlinkedin'
+      },
+    } as IProfile,
+    title: 'Edit Public Profile'
+  };
 
   beforeEach(() => {
+    mockDialogRefSpy = jasmine.createSpyObj('MatDialogRef', ['close']);
+
     TestBed.configureTestingModule({
       declarations: [ProfileFormDialogComponent],
       imports: [HttpClientTestingModule, ReactiveFormsModule],
       providers: [
-        { provide: MatDialogRef, useValue: { close: () => { } } },
-        { provide: MAT_DIALOG_DATA, useValue: {} }
+        { provide: MatDialogRef, useValue: mockDialogRefSpy },
+        { provide: MAT_DIALOG_DATA, useValue: {} },
       ],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA]
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     });
     fixture = TestBed.createComponent(ProfileFormDialogComponent);
     component = fixture.componentInstance;
@@ -27,5 +50,23 @@ describe('ProfileFormDialogComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should be in edit mode when data has edit mode', () => {
+    component.profileData = mockModeEditData;
+    expect(component.profileData.mode).toBe('edit');
+
+    component.ngOnInit();
+
+    expect(component.isEditMode).toBeTrue();
+  });
+
+  it('should be in create mode when data has create mode', () => {
+    component.profileData = mockModeCreateData;
+    expect(component.profileData.mode).toBe('create');
+
+    component.ngOnInit();
+
+    expect(component.isEditMode).toBeFalse();
   });
 });
