90a4bb85a890314f5f41ad4ead306dd9acf96e27
Zara <zl.zlatkova@gmail.com>
2025-08-08 13:22:32 +0200

    Аdd dialog data mock and dialog behavior tests

--- client/src/app/shared/confirm-dialog/confirm-dialog.component.spec.ts
+++ client/src/app/shared/confirm-dialog/confirm-dialog.component.spec.ts
@@ -5,21 +5,31 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { IConfirmDialogData } from 'src/app/interfaces/dialogData';
 
 describe('ConfirmDialogComponent', () => {
   let component: ConfirmDialogComponent;
   let fixture: ComponentFixture<ConfirmDialogComponent>;
   let mockDialogRefSpy: jasmine.SpyObj<MatDialogRef<ConfirmDialogComponent>>;
 
+  const mockDialogDataSpy: IConfirmDialogData = {
+    title: '',
+    message: '',
+    confirmButtonText: '',
+    cancelButtonText: '',
+    emailInputTag: false
+  }
+
   beforeEach(() => {
     mockDialogRefSpy = jasmine.createSpyObj('MatDialogRef', ['close']);
 
     TestBed.configureTestingModule({
       declarations: [ConfirmDialogComponent],
-      imports: [HttpClientTestingModule, RouterTestingModule],
+      imports: [HttpClientTestingModule, RouterTestingModule, ReactiveFormsModule],
       providers: [
         { provide: MatDialogRef, useValue: mockDialogRefSpy },
-        { provide: MAT_DIALOG_DATA, useValue: {} },
+        { provide: MAT_DIALOG_DATA, useValue: mockDialogDataSpy },
       ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     });
@@ -34,8 +44,11 @@ describe('ConfirmDialogComponent', () => {
 
 
   it('should close dialog with true when emailInputTag is true', () => {
-    const emailControl = component.emailForm.get('email')
+    mockDialogDataSpy.emailInputTag = true;
+
+    const emailControl = component.emailForm.get('email');
     emailControl?.setValue('test@domain.com');
+    emailControl?.markAsTouched();
 
     fixture.detectChanges();
 
@@ -45,6 +58,8 @@ describe('ConfirmDialogComponent', () => {
   });
 
   it('should close dialog with true when emailInputTag is false', () => {
+    mockDialogDataSpy.emailInputTag = false;
+
     component.onConfirm();
     expect(mockDialogRefSpy.close).toHaveBeenCalledWith(true);
 
@@ -55,6 +70,70 @@ describe('ConfirmDialogComponent', () => {
     expect(mockDialogRefSpy.close).toHaveBeenCalledWith(false);
 
   });
+
+  it('should show validation errors when field is touched and empty', () => {
+    mockDialogDataSpy.emailInputTag = true;
+
+    const emailControl = component.emailForm.get('email');
+    emailControl?.setValue('');
+    emailControl?.markAsTouched();
+
+    fixture.detectChanges();
+
+    const errorMessages = fixture.nativeElement.querySelector('[data-testid="error-msg-required"]');
+
+    expect(errorMessages).toBeTruthy();
+    expect(errorMessages.textContent.trim()).toContain('Email is required!');
+
+  });
+
+  it('should show validation errors for invalid email when touched', () => {
+    mockDialogDataSpy.emailInputTag = true;
+
+    const emailControl = component.emailForm.get('email');
+    emailControl?.setValue('test@domain');
+    emailControl?.markAsTouched();
+
+    fixture.detectChanges();
+
+
+    const errorMessages = fixture.nativeElement.querySelector('[data-testid="error-msg"]');
+
+    expect(errorMessages).toBeTruthy();
+    expect(errorMessages.textContent.trim()).toContain('Please enter a valid email address');
+
+  });
+
+  it('should display valid button text onCancel', () => {
+    mockDialogDataSpy.cancelButtonText = 'Cancel';
+
+    fixture.detectChanges();
+
+
+    const cancelBtn = fixture.nativeElement.querySelector('[data-testid="cancel-btn"]');
+    cancelBtn.click();
+
+    expect(cancelBtn).toBeTruthy();
+    expect(cancelBtn.textContent.trim()).toContain('Cancel');
+
+  });
+
+  it('should display valid button text onConfirm', () => {
+    mockDialogDataSpy.confirmButtonText = 'Unsubscribe';
+
+    fixture.detectChanges();
+
+
+    const confirmBtn = fixture.nativeElement.querySelector('[data-testid="confirm-btn"]');
+    confirmBtn.click();
+
+    expect(confirmBtn).toBeTruthy();
+    expect(confirmBtn.textContent.trim()).toContain('Unsubscribe');
+
+  });
+
+
+
 
 
 });
