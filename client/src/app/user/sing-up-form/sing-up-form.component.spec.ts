61e672338f3e160237dadaf23d0183e10a25fa63
Zara <zl.zlatkova@gmail.com>
2025-08-08 17:26:05 +0200

    Add tests for form password group visibility toggle

--- client/src/app/user/sing-up-form/sing-up-form.component.spec.ts
+++ client/src/app/user/sing-up-form/sing-up-form.component.spec.ts
@@ -2,7 +2,6 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SingUpFormComponent } from './sing-up-form.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 
@@ -14,12 +13,8 @@ describe('SingUpFormComponent', () => {
     TestBed.configureTestingModule({
       declarations: [SingUpFormComponent],
       imports: [HttpClientTestingModule, ReactiveFormsModule],
-      providers: [
-      { provide: MatDialogRef, useValue: { close: () => {} } },
-      { provide: MAT_DIALOG_DATA, useValue: {} }
-    ],
-    schemas: [CUSTOM_ELEMENTS_SCHEMA]
-       
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
+
     });
     fixture = TestBed.createComponent(SingUpFormComponent);
     component = fixture.componentInstance;
@@ -29,4 +24,79 @@ describe('SingUpFormComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should show password when togglePasswordVisibility is called', () => {
+    component.showPassword = false;
+    fixture.detectChanges();
+
+    const passIsHidden = fixture.nativeElement.querySelector('input[data-testid="input-pass"][type="password"]');
+    expect(passIsHidden).toBeTruthy();
+
+    component.togglePasswordVisibility();
+    fixture.detectChanges();
+
+
+    const passIsVisible = fixture.nativeElement.querySelector('input[data-testid="input-pass"][type="text"]');
+    expect(passIsVisible).toBeTruthy();
+
+    expect(component.showPassword).toBeTrue();
+
+  });
+
+
+  it('should hidden password when togglePasswordVisibility is called', () => {
+    component.showPassword = true;
+    fixture.detectChanges();
+
+    const passIsVisible = fixture.nativeElement.querySelector('input[data-testid="input-pass"][type="text"]');
+    expect(passIsVisible).toBeTruthy();
+
+    component.togglePasswordVisibility();
+    fixture.detectChanges();
+
+    const passIsHidden = fixture.nativeElement.querySelector('input[data-testid="input-pass"][type="password"]');
+    expect(passIsHidden).toBeTruthy();
+
+    expect(component.showPassword).toBeFalse();
+
+  });
+
+
+  it('should show rePassword when toggleConfirmPasswordVisibility is called', () => {
+    component.showConfirmPassword = false;
+    fixture.detectChanges();
+
+    const passIsHidden = fixture.nativeElement.querySelector('input[data-testid="input-repass"][type="password"]');
+    expect(passIsHidden).toBeTruthy();
+
+    component.toggleConfirmPasswordVisibility();
+    fixture.detectChanges();
+
+
+    const passIsVisible = fixture.nativeElement.querySelector('input[data-testid="input-repass"][type="text"]');
+    expect(passIsVisible).toBeTruthy();
+
+    expect(component.showConfirmPassword).toBeTrue();
+
+
+  });
+
+  it('should hidden password when toggleConfirmPasswordVisibility is called', () => {
+    component.showConfirmPassword = true;
+    fixture.detectChanges();
+
+    const passIsVisible = fixture.nativeElement.querySelector('input[data-testid="input-repass"][type="text"]');
+    expect(passIsVisible).toBeTruthy();
+
+    component.toggleConfirmPasswordVisibility();
+    fixture.detectChanges();
+
+    const passIsHidden = fixture.nativeElement.querySelector('input[data-testid="input-repass"][type="password"]');
+    expect(passIsHidden).toBeTruthy();
+
+    expect(component.showConfirmPassword).toBeFalse();
+
+  });
+
+
 });
