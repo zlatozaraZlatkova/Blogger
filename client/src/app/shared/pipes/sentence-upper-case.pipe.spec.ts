08b55285b6f8fc837b2be91159e3803a009bccc9
Zara <zl.zlatkova@gmail.com>
2025-08-09 10:43:59 +0200

    Add unit tests for SentenceUpperCasePipe

--- client/src/app/shared/pipes/sentence-upper-case.pipe.spec.ts
+++ client/src/app/shared/pipes/sentence-upper-case.pipe.spec.ts
@@ -1,8 +1,29 @@
 import { SentenceUpperCasePipe } from './sentence-upper-case.pipe';
 
 describe('SentenceUpperCasePipe', () => {
+  let pipe: SentenceUpperCasePipe;
+
+  beforeEach(() => {
+    pipe = new SentenceUpperCasePipe();
+  });
+
   it('create an instance', () => {
-    const pipe = new SentenceUpperCasePipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should transform fist letter to uppercase', () => {
+    expect(pipe.transform('abc')).toBe('Abc');
+  });
+
+  it('should transform first letter uppercase, rest lowercase', () => {
+    expect(pipe.transform('aBc')).toBe('Abc');
+  });
+
+  it('should make only first letter uppercase when input is all uppercase', () => {
+    expect(pipe.transform('ABC')).toBe('Abc');
+  });
+
+  it('should return empty string when input is empty string', () => {
+    expect(pipe.transform('')).toBe('');
   });
 });
