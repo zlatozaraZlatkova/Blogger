1b8f7ac8292cae233852e7bd52c1257cb6a3bc37
Zara <zl.zlatkova@gmail.com>
2025-08-09 11:45:08 +0200

    Setup service testing with HTTP and data mocks

--- client/src/app/features/profile/profile-api.service.spec.ts
+++ client/src/app/features/profile/profile-api.service.spec.ts
@@ -1,19 +1,49 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ProfileApiService } from './profile-api.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { IProfile } from 'src/app/interfaces/profile';
 
 describe('ProfileApiService', () => {
   let service: ProfileApiService;
+  let httpMock: HttpTestingController;
+
+  const mockProfileData: IProfile = {
+    _id: '1',
+    ownerId: 'owner-id-123',
+    bio: 'Test bio content',
+    githubUsername: 'testuser',
+    socialMedia: {
+      linkedin: 'testlinkedin'
+    },
+    createdAt: '2025-08-01',
+    updatedAt: '2025-08-08'
+  }
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule] 
+      imports: [HttpClientTestingModule],
+      providers: [ProfileApiService]
     });
     service = TestBed.inject(ProfileApiService);
+    httpMock = TestBed.inject(HttpTestingController);
+
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should call getUserPublicProfile and return data', () => {
+    service.getUserPublicProfile().subscribe(data => {
+      expect(data).toEqual(mockProfileData);
+    });
+
+    const req = httpMock.expectOne('/api/profile');
+    expect(req.request.method).toBe('GET');
+    req.flush(mockProfileData);
+  });
+
+
+
 });
