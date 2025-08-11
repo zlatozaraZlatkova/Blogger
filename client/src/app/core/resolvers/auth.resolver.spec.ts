f3bca913a6eac0a8342bc2322ce3cf52975f6473
Zara <zl.zlatkova@gmail.com>
2025-08-11 19:26:51 +0200

    Add unit test to verify resolver calls

--- client/src/app/core/resolvers/auth.resolver.spec.ts
+++ client/src/app/core/resolvers/auth.resolver.spec.ts
@@ -1,18 +1,68 @@
 import { TestBed } from '@angular/core/testing';
-import { ResolveFn } from '@angular/router';
+import { ActivatedRouteSnapshot, ResolveFn, RouterStateSnapshot } from '@angular/router';
 
 import { authResolver } from './auth.resolver';
 import { IUser } from 'src/app/interfaces/user';
+import { AuthService } from 'src/app/user/auth.service';
+import { Observable, of } from 'rxjs';
 
 describe('authResolver', () => {
-  const executeResolver: ResolveFn<IUser> = (...resolverParameters) => 
-      TestBed.runInInjectionContext(() => authResolver(...resolverParameters));
+  const executeResolver: ResolveFn<IUser> = (...resolverParameters) =>
+    TestBed.runInInjectionContext(() => authResolver(...resolverParameters));
+
+  let authServiceSpy: jasmine.SpyObj<AuthService>;
+
+  const mockUserData: IUser = {
+    _id: '123',
+    name: 'User Name',
+    email: 'username@domain.com',
+    avatar: 'www.avatar.com',
+    createdPosts: [],
+    likedPostList: [],
+    followedUsersList: [],
+  };
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    authServiceSpy = jasmine.createSpyObj('AuthService', ['checkIsUserAuthenticated']);
+    authServiceSpy.checkIsUserAuthenticated.and.returnValue(of(mockUserData));
+
+    TestBed.configureTestingModule({
+      providers: [{ provide: AuthService, useValue: authServiceSpy }],
+    });
+
   });
 
   it('should be created', () => {
     expect(executeResolver).toBeTruthy();
   });
+
+  it('should call checkIsUserAuthenticated and resolve user data', () => {
+
+    const mockRoute = {
+      params: {},
+      data: {
+        title: "User's Profile",
+        layout: 'default',
+        showHeader: true,
+        showFooter: true,
+      },
+    } as any as ActivatedRouteSnapshot;
+
+    const mockState = {
+      url: '/profile',
+      root: mockRoute,
+    } as any as RouterStateSnapshot;
+
+    authServiceSpy.checkIsUserAuthenticated.and.returnValue(of(mockUserData));
+
+    TestBed.runInInjectionContext(() => {
+      return (authResolver(mockRoute, mockState) as Observable<IUser>)
+        .subscribe((user) => {
+          expect(user).toEqual(mockUserData);
+        });
+    });
+
+    expect(authServiceSpy.checkIsUserAuthenticated).toHaveBeenCalled();
+  });
+
 });
