158fb5c2afee949a13cc577a58c2fc4ab30e62af
Zara <zl.zlatkova@gmail.com>
2025-08-12 17:04:18 +0200

    Setup TestBed configuration and add baseline unit test  for UserProfileComponent

--- client/src/app/features/profile/user-profile/user-profile.component.spec.ts
+++ client/src/app/features/profile/user-profile/user-profile.component.spec.ts
@@ -5,17 +5,64 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ActivatedRoute } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { of } from 'rxjs';
+import { BehaviorSubject, of } from 'rxjs';
+import { IProfile } from 'src/app/interfaces/profile';
+import { ProfileService } from '../profile.service';
+import { AuthService } from 'src/app/user/auth.service';
+import { IUser } from 'src/app/interfaces/user';
+import { IPost } from 'src/app/interfaces/post';
 
 describe('UserProfileComponent', () => {
   let component: UserProfileComponent;
   let fixture: ComponentFixture<UserProfileComponent>;
+  let profileServiceSpy: jasmine.SpyObj<ProfileService>;
+
+  let profileSubject = new BehaviorSubject<IProfile | null>(null);
+  let userSubject = new BehaviorSubject<IUser | null>(null);
+
+  const mockProfileData: IProfile = {
+    _id: '1',
+    ownerId: 'owner-id-123',
+    bio: 'Test bio content',
+    githubUsername: 'testuser',
+    socialMedia: {
+      linkedin: 'testlinkedin'
+    },
+    followerList: [],
+    createdAt: '2025-08-01',
+    updatedAt: '2025-08-08'
+  }
+
+  const mockUserData: IUser = {
+    _id: '1',
+    name: 'User Name',
+    email: 'user@email.com',
+    avatar: 'www.avatar.com',
+    createdPosts: [],
+    likedPostList: [],
+    followedUsersList: []
+  };
 
   beforeEach(() => {
+    profileSubject = new BehaviorSubject<IProfile | null>(mockProfileData);
+
+    profileServiceSpy = jasmine.createSpyObj('ProfileService', ['getProfile'], {
+      userPublicProfile$: profileSubject.asObservable()
+    })
+
+    userSubject = new BehaviorSubject<IUser | null>(mockUserData);
+
+    const authServiceSpy = jasmine.createSpyObj('AuthService', [], {
+      user$: userSubject.asObservable()
+    });
+
+
     TestBed.configureTestingModule({
       declarations: [UserProfileComponent],
       imports: [HttpClientTestingModule, ReactiveFormsModule],
       providers: [
+        { provide: ProfileService, useValue: profileServiceSpy },
+        { provide: AuthService, useValue: authServiceSpy },
         {
           provide: ActivatedRoute,
           useValue: {
@@ -39,4 +86,15 @@ describe('UserProfileComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should validate created posts from mock user data', () => {
+    let result: IPost[] = [];
+
+    component.userCreatedPosts$.subscribe(posts => result = posts);
+
+    fixture.detectChanges();
+
+    expect(result).toEqual([]);
+  });
+
 });
